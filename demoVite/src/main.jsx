12300a7ff2511dfec2c58399364ad833e0b412e1
tribecon1 <bentl24@byu.edu>
2024-10-23 11:47:01 -0600

    more React practice, commented out for now!

--- demoVite/src/main.jsx
+++ demoVite/src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
+import { StrictMode, createElement } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import Accordion from './Accordion.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    {/*<Accordion />*/}
   </StrictMode>,
 )
