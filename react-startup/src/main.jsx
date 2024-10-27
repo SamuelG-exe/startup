440e05237e4c44dad7a1be89b5987d6f7ffacd4c
tribecon1 <bentl24@byu.edu>
2024-10-26 19:05:23 -0600

    header and footer hold size, just can't change the styling

--- react-startup/src/main.jsx
+++ react-startup/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
+import './css_components/index.css'
 // Import Bootstrap first
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import 'bootstrap/dist/css/bootstrap.min.css';
