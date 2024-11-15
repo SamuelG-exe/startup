ff0fc44025787e813baa3e052fe73ea05535dc88
tribecon1 <bentl24@byu.edu>
2024-11-15 09:33:31 -0700

    assignment instructions

--- react-startup/service/index.js
+++ react-startup/service/index.js
@@ -8,6 +8,7 @@ const port = process.argv.length > 2 ? process.argv[2] : 4000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static('public'));
 
 var apiRouter = express.Router();
 app.use(`/api`, apiRouter);
